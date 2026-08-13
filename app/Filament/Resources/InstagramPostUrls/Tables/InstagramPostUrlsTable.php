0b825c99dd3f80241649ca792976b83ee6e2a70a
Andri Efendy <103976426+andriefendy26@users.noreply.github.com>
2026-08-13 10:18:35 +0800

    update

--- app/Filament/Resources/InstagramPostUrls/Tables/InstagramPostUrlsTable.php
+++ app/Filament/Resources/InstagramPostUrls/Tables/InstagramPostUrlsTable.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\InstagramPostUrls\Tables;
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
+use Filament\Actions\DeleteAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
@@ -30,6 +31,7 @@ class InstagramPostUrlsTable
             ])
             ->recordActions([
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
