27b5866dd1c0c6049f5cc5f982b4f2e150778f0e
Andri Efendy <103976426+andriefendy26@users.noreply.github.com>
2026-08-19 10:05:47 +0800

    update profile

--- resources/views/pages/profil-struktur.blade.php
+++ resources/views/pages/profil-struktur.blade.php
@@ -32,5 +32,23 @@
             </div>
         </div>
     </div>
+
+    <div class="mx-auto max-w-7xl px-4 pb-16 md:pb-24">
+        <div class="mx-auto max-w-4xl">
+            <div x-data="{ zoomed: false }" class="rounded-2xl border border-neutral-200 bg-neutral-50 p-4 dark:border-neutral-700 dark:bg-neutral-900">
+                <img src="{{ asset('storage/profil/struktur/struktur.png') }}" alt="Struktur Organisasi Puskesmas Pantai Amal" class="w-full h-auto rounded-xl cursor-zoom-in object-cover" @click="zoomed = true">
+                <p class="mt-3 text-center text-sm font-medium text-neutral-600 dark:text-neutral-300">Struktur Organisasi Puskesmas Pantai Amal</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div x-show="zoomed" x-cloak x-transition.opacity class="fixed inset-0 z-[9999] flex items-center justify-center bg-black/80 p-4" @click.self="zoomed = false">
+    <div class="relative max-h-[90vh] max-w-[90vw] overflow-auto rounded-xl bg-white p-2 dark:bg-neutral-900">
+        <button @click="zoomed = false" class="absolute right-4 top-4 rounded-full bg-white/90 p-2 text-neutral-700 shadow-lg transition-colors hover:bg-white dark:bg-neutral-800 dark:text-neutral-200 dark:hover:bg-neutral-700" aria-label="Tutup">
+            <i data-lucide="x" class="h-5 w-5"></i>
+        </button>
+        <img src="{{ asset('storage/profil/struktur/struktur.png') }}" alt="Struktur Organisasi Puskesmas Pantai Amal" class="max-h-[85vh] w-auto rounded-lg object-contain">
+    </div>
 </div>
 @endsection
